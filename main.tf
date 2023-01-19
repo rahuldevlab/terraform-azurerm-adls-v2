06f26c7732e33d40a0debbff65e565cf9b9196f9
omartushevskyi <oleh_martushevskyi@epam.com>
2023-01-19 13:52:05 -0500

    fix: formatting

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ locals {
 resource "azurerm_storage_data_lake_gen2_filesystem" "this" {
   name               = var.name
   storage_account_id = var.storage_account_id
-  properties         = {
+  properties = {
     for key, value in var.properties: key => base64encode(value)
   }
 
