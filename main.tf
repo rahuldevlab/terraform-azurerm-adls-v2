15bd7053d2047010665fa4621a03bf51e9bd5466
omartushevskyi <oleh_martushevskyi@epam.com>
2023-01-19 13:54:57 -0500

    fix: formatting

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "this" {
   name               = var.name
   storage_account_id = var.storage_account_id
   properties = {
-    for key, value in var.properties: key => base64encode(value)
+    for key, value in var.properties : key => base64encode(value)
   }
 
   lifecycle { prevent_destroy = false }
